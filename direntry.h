807b8c269c2d330e3c7275d2bc0469980105ae82
JoaquinBTH <jolk19@student.bth.se>
2023-01-08 13:56:15 +0100

    Initial commit

---
+++ direntry.h
@@ -0,0 +1,7 @@
+struct dir_entry {
+    char file_name[56]; // name of the file / sub-directory
+    uint32_t size; // size of the file in bytes
+    uint16_t first_blk; // index in the FAT for the first block of the file
+    uint8_t type; // directory (1) or file (0)
+    uint8_t access_rights; // read (0x04), write (0x02), execute (0x01)
+};
